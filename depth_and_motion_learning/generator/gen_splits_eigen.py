21339c80f2582d66d522a8e706702cb4379ffc62
carloradice <c.radice@campus.unimib.it>
2021-12-28 16:24:58 +0100

    fixed wrong test path

--- depth_and_motion_learning/generator/gen_splits_eigen.py
+++ depth_and_motion_learning/generator/gen_splits_eigen.py
@@ -1,12 +1,11 @@
 import os
 import random
 
-DEFAULT_PATH = '/media/RAIDONE/radice/datasets/kitti/struct2depth'
 INPUT_DIR = '/media/RAIDONE/radice/datasets/kitti/'
 OUTPUT_DIR = '/media/RAIDONE/radice/neural-networks-data/depth-and-motion-learning/splits/kitti/'
 
 
-def fill_data(lines):
+def fill_data(lines, type):
     data = []
     for line in lines:
         path = line.rstrip().split()[0]
@@ -18,12 +17,20 @@ def fill_data(lines):
         basename = path[4].split('.')[0]
 
         # Se esiste il file, aggiungerlo allo split
-        f = os.path.join(DEFAULT_PATH, date, sequence, 'image_02', '{}.png'.format(basename))
-        if os.path.isfile(f):
-            l = os.path.join(DEFAULT_PATH, date, sequence, 'image_02') + ' ' + '{}{}'.format(basename, '\n')
-            r = os.path.join(DEFAULT_PATH, date, sequence, 'image_03') + ' ' + '{}{}'.format(basename, '\n')
-            data.append(l)
-            data.append(r)
+        if type == 'train':
+            f = os.path.join(INPUT_DIR, 'struct2depth', date, sequence, 'image_02', '{}.png'.format(basename))
+            if os.path.isfile(f):
+                l = os.path.join(INPUT_DIR, 'struct2depth', date, sequence, 'image_02') + ' ' + '{}{}'.format(basename, '\n')
+                r = os.path.join(INPUT_DIR, 'struct2depth', date, sequence, 'image_03') + ' ' + '{}{}'.format(basename, '\n')
+                data.append(l)
+                data.append(r)
+
+        if type == 'test':
+                l = os.path.join(INPUT_DIR, 'data', date, sequence, 'image_02', 'data') + ' ' + '{}{}'.format(basename, '\n')
+                r = os.path.join(INPUT_DIR, 'data', date, sequence, 'image_03', 'data') + ' ' + '{}{}'.format(basename, '\n')
+                data.append(l)
+                data.append(r)
+
 
     return data
 
@@ -32,7 +39,7 @@ def gen_splits():
     # Train
     file = open(INPUT_DIR + 'eigen_train_files.txt', 'r')
     lines = file.readlines()
-    train = fill_data(lines=lines)
+    train = fill_data(lines=lines, type='train')
     random.shuffle(train)
     file = open(OUTPUT_DIR + 'eigen_train_files.txt', 'w')
     file.writelines(train)
@@ -40,7 +47,7 @@ def gen_splits():
     # Test
     file = open(INPUT_DIR + 'eigen_test_files.txt', 'r')
     lines = file.readlines()
-    test = fill_data(lines=lines)
+    test = fill_data(lines=lines, type='test')
     random.shuffle(test)
     file = open(OUTPUT_DIR + 'eigen_test_files.txt', 'w')
     file.writelines(test)
