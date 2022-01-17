791c1ae831936ccf565575c4ed1e972fae74737f
carloradice <c.radice@campus.unimib.it>
2022-01-17 17:14:19 +0100

    added comments

--- depth_and_motion_learning/generator/gen_splits_eigen.py
+++ depth_and_motion_learning/generator/gen_splits_eigen.py
@@ -14,7 +14,7 @@ def parse_args():
     return parser.parse_args()
 
 
-def fill_data(lines, type, folder):
+def fill_data(lines, type, folder=None):
     data = []
     for line in lines:
         path = line.rstrip().split()[0]
@@ -22,10 +22,12 @@ def fill_data(lines, type, folder):
 
         date = path[0]
         sequence = path[1]
-        # folder = 'image_02' or 'image_s03'
+        # folder = 'image_02' or 'image_03'
         basename = path[4].split('.')[0]
 
         # Se esiste il file, aggiungerlo allo split
+        # Ci sono meno di 22600 * 2 files poichè se il file è il primo o l'ultimo di una sequenza non viene
+        # considerato in quanto la tripletta non avrebbe una delle 3 immagini
         if type == 'train':
             f = os.path.join(INPUT_DIR, folder, date, sequence, 'image_02', '{}.png'.format(basename))
             if os.path.isfile(f):
@@ -35,10 +37,10 @@ def fill_data(lines, type, folder):
                 data.append(r)
 
         if type == 'test':
-                l = os.path.join(INPUT_DIR, 'data', date, sequence, 'image_02', 'data') + ' ' + '{}{}'.format(basename, '\n')
-                r = os.path.join(INPUT_DIR, 'data', date, sequence, 'image_03', 'data') + ' ' + '{}{}'.format(basename, '\n')
-                data.append(l)
-                data.append(r)
+            l = os.path.join(INPUT_DIR, 'data', date, sequence, 'image_02', 'data') + ' ' + '{}{}'.format(basename, '\n')
+            r = os.path.join(INPUT_DIR, 'data', date, sequence, 'image_03', 'data') + ' ' + '{}{}'.format(basename, '\n')
+            data.append(l)
+            data.append(r)
 
 
     return data
@@ -53,7 +55,9 @@ def gen_splits():
     # Train
     file = open(INPUT_DIR + 'eigen_train_files.txt', 'r')
     lines = file.readlines()
+    print(len(lines))
     train = fill_data(lines=lines, type='train', folder=folder)
+    print(len(train))
     random.shuffle(train)
     file = open(OUTPUT_DIR + 'eigen_train_files_' + sub + '.txt', 'w')
     file.writelines(train)
@@ -61,9 +65,9 @@ def gen_splits():
     # Test
     file = open(INPUT_DIR + 'eigen_test_files.txt', 'r')
     lines = file.readlines()
-    test = fill_data(lines=lines, type='test', folder=folder)
+    test = fill_data(lines=lines, type='test')
     random.shuffle(test)
-    file = open(OUTPUT_DIR + 'eigen_test_files_' + sub + '.txt', 'w')
+    file = open(OUTPUT_DIR + 'eigen_test_files.txt', 'w')
     file.writelines(test)
 
 
